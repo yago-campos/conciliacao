de00a2a0dfbe49ab75de4d7cf873f02fbe7b23bb
Yago Campos <107326810+yago-campos@users.noreply.github.com>
2024-11-25 12:35:34 -0300

    Update funcoesDeConcilia.js

--- pages/api/funcoesDeConcilia.js
+++ pages/api/funcoesDeConcilia.js
@@ -1,12 +1,12 @@
 // pages/api/funcoesDeConcilia.js
-import * as conciliaApsenDimed from '../scripts/conciliacao_apsen_dimed.py';
-import * as conciliaApsenGam from '../scripts/conciliacao_apsen_gam.py';
-import * as conciliaApsenGjb from '../scripts/conciliacao_apsen_gjb.py';
-import * as conciliaApsenGsc from '../scripts/conciliacao_apsen_gsc.py';
-import * as conciliaMsdAgille from '../scripts/conciliacao_msd_agille.py';
-import * as conciliaMsdGsc from '../scripts/conciliacao_msd_gsc.py';
-import * as conciliaOrganonGsc from '../scripts/conciliacao_organon_gsc.py';
-import * as conciliaBayerGsc from '../scripts/conciliacao_bayer_gsc.py';
+import * as conciliaApsenDimed from '../scripts/conciliação_apsen_dimed';
+import * as conciliaApsenGam from '../scripts/conciliação_apsen_gam';
+import * as conciliaApsenGjb from '../scripts/conciliação_apsen_gjb';
+import * as conciliaApsenGsc from '../scripts/conciliação_apsen_gsc';
+import * as conciliaMsdAgille from '../scripts/conciliação_msd_agille';
+import * as conciliaMsdGsc from '../scripts/conciliação_msd_gsc';
+import * as conciliaOrganonGsc from '../scripts/conciliação_organon_gsc';
+import * as conciliaBayerGsc from '../scripts/conciliação_bayer_gsc';
 
 export async function processarConciliação(baseFuncional, baseDistribuidor, pfMargem, industria, distribuidor) {
   try {
